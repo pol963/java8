3bcd6e6524831b416070df925ba36f346bf63073
pol963 <pol963@naver.com>
2022-11-25 20:19:19 +0900

    22-11-25-01

---
+++ java8to11/src/main/java/me/whiteship/javaTest7/InnerClass.java
@@ -0,0 +1,16 @@
+package me.whiteship.javaTest7;
+
+class A{
+    int i = 10;
+    class B{
+        public void method(){
+            System.out.println("i : " + i);
+        }
+    }
+}
+
+public class InnerClass {
+    public static void main(String[] args) {
+
+    }
+}
