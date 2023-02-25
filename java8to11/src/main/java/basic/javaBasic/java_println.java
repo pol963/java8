27ad16157b70a5068915e6ce5201c675aac7f94f
pol963 <pol963@naver.com>
2023-02-26 05:38:57 +0900

    22-02-26-01

---
+++ java8to11/src/main/java/basic/javaBasic/java_println.java
@@ -0,0 +1,15 @@
+package basic.javaBasic;
+
+import org.springframework.boot.autoconfigure.gson.GsonProperties;
+
+public class java_println {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.print("k");
+        System.out.print("k");
+        System.out.print("k");
+
+    }
+}
--- java8to11/src/main/java/me/whiteship/javaTest8/Function_Interface.java
+++ java8to11/src/main/java/me/whiteship/javaTest8/Function_Interface.java
@@ -2,43 +2,18 @@ package me.whiteship.javaTest8;
 
 public class Function_Interface {
 
-    static MyFunction2 getMyFunction(){ //반환타입이 MyFunction2 인 메서드.
-        MyFunction2 f = () -> System.out.println("f3.run");
-        return f;
+    @FunctionalInterface
+    interface MyFunctionTest{
+        int max(int a, int b);
     }
-
     public static void main(String[] args) {
 
-        /* 람다 사용 전.
-         MyFunction f = new MyFunction(){
-             public int max(int a, int b){
-                 return a > b ? a : b;
-             }
-         };
-        */
+        MyFunctionTest ft = (a,b) -> a > b ? a : b;
 
-        //위의 코드를 람다로 간편하게 변경
-        MyFunction f = (a, b) -> a > b ? a : b;
-        int value = f.max(3,4);
+        int value = ft.max(5,7);
         System.out.println(value);
-
-        MyFunction2 f2 = () -> System.out.println("f2.run()");
-        f2.run();
-
-        MyFunction2 f3 = getMyFunction();
-        f3.run();
 
     }
 }
 
-@FunctionalInterface
-interface MyFunction{
-    public abstract int max(int a, int b); // -> int max(int a, int b) 와 동일 public abstract 생략가능.
-
-}
-
-@FunctionalInterface
-interface MyFunction2{
-    void run();
-}
 
