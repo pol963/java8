3f4015e6d05185b2c861ee328b88067e618fe755
pol963 <pol963@naver.com>
2023-03-04 06:06:15 +0900

    22-03-04-01

--- java8to11/src/main/java/basic/javaBasic/java_println.java
+++ java8to11/src/main/java/basic/javaBasic/java_println.java
@@ -11,5 +11,12 @@ public class java_println {
         System.out.print("k");
         System.out.print("k");
 
+        //변수와 출력방법.
+        int age = 23, num = 3;
+        String name = "Kang man gi";
+
+        int total = age+num;
+        System.out.println("total : " + total);
+
     }
 }
---
+++ java8to11/src/main/java/basic/javaBasic/testbasic.java
@@ -0,0 +1,12 @@
+package basic.javaBasic;
+
+public class testbasic {
+    public static void main(String[] args) {
+        int age = 23, num = 3;
+        String name = "Kang man gi";
+
+        int total = age+num;
+        System.out.println("total : " + total);
+
+    }
+}
