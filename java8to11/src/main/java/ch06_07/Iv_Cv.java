2a07b291d91d02ad717fd52aee5c4f682b1cc171
pol963 <pol963@naver.com>
2023-04-06 13:16:11 +0900

    23-04-06-01

--- java8to11/src/main/java/ch06_07/Iv_Cv.java
+++ java8to11/src/main/java/ch06_07/Iv_Cv.java
@@ -15,14 +15,14 @@ public class Iv_Cv {
         System.out.println("c1.num = " + c1.num);
         System.out.println("c2.kind = " + c2.kind);
         System.out.println("c2.num = " + c2.num);
-        System.out.println("c1.width = " + c1.width);
-        System.out.println("c2.height = " + c2.height);
+        System.out.println("c1.width = " + Card.width);
+        System.out.println("c2.height = " + Card.height);
 
-        c1.width = 120;
-        c2.height = 270;
+        Card.width = 120;
+        Card.height = 270;
 
-        System.out.println("변경후 c2.width = " + c2.width);
-        System.out.println("변경후 c1.height = " + c1.height);
+        System.out.println("변경후 c2.Card = " + Card.width);
+        System.out.println("변경후 c1.Card = " + Card.height);
 
         //인스턴스 변수(iv) -> 각 객체마다 다른 공간을 가지고있음 -> 독립된 값 지정 가능.
         //클래스 변수(cv) -> 모든 객체가 하나의 저장공간을 공유 -> 항상 공통된 값을 가지고 있음.
