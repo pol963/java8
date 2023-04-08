97d8248c48111884053fff40929df40379a5f2cb
pol963 <pol963@naver.com>
2023-04-09 03:47:43 +0900

    23-04-09-01

--- java8to11/src/main/java/ch06_07/Constructor.java
+++ java8to11/src/main/java/ch06_07/Constructor.java
@@ -1,6 +1,7 @@
 package ch06_07;
 
 class Data1{
+    //컴파일러가 자동으로 기본생성자 추가.
     int value;
 }
 class Data2{
@@ -15,7 +16,7 @@ public class Constructor {
     public static void main(String[] args) {
         Data1 d1 = new Data1();
 
-        //Data2 d2 = new Data2(); 에러 기본생성자가 아닌 만든 생성자를 사용해야합니다.
+        //Data2 d2 = new Data2(); 에러 기본생성자가 없습니다. 생성하거나 혹은 미리 만들어둔 생성자를 사용해야 합니다.
         Data2 d2 = new Data2(3);
     }
 }
